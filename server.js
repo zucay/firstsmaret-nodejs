447d7266cc72e9c96f44026d5dbda614b983a5dc
yukihico <yukihico@gmail.com>
2015-08-01 10:41:56 +0900

    bugfix

--- server.js
+++ server.js
@@ -11,14 +11,20 @@ var http = require('http').createServer(function(req, res) {
 });
 
 var io = require('socket.io')(http);
+var room = null;
 http.listen(listenPort);
 io.on('connection', function(socket) {
+  //console.log('one user connected: ' + socket.id);
   socket.on('joinRoom', function(roomName) {
-    var room = socket.join(roomName);
-    console.log('joinned room: ' + roomName);
+    room = socket.join(roomName);
+    //console.log('joinned room: ' + roomName + '(' + room.rooms + ')');
     room.on('msg', function(data) {
-      room.emit('msg', data);
+      //room.emit('msg', data); //明示的にio.to()して部屋を指定しないと、そのroomで接続しているユーザに通知できなかった。
+      io.to(roomName).emit('msg', data);
       console.log(data);
     });
+  });
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
   });
 });
